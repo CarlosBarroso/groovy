80fb9020190547300513ba0c72238c5c2a0d9d1c
CarlosBarroso <cbc_sg@yahoo.es>
2021-07-15 10:17:10 +0200

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,21 +1,21 @@
 def call() {
-  pipeline {
-    agent {
-        docker {
-            image 'mcr.microsoft.com/dotnet/core/sdk:3.1.101'
-        }
-    }
-    stages {
-        stage('verify') {
-            steps {
-                sh '''
-                  dotnet --list-sdks
-                  dotnet --list-runtimes
-                '''
-                sh 'printenv'
-//                sh 'ls -l "$WORKSPACE"'
+    pipeline {
+        agent {
+            docker {
+                image 'mcr.microsoft.com/dotnet/core/sdk:3.1.101'
             }
         }
+        stages {
+            stage('verify') {
+                steps {
+                    sh '''
+                        dotnet --list-sdks
+                        dotnet --list-runtimes
+                    '''
+                    sh 'printenv'
+                    sh 'ls -l "$WORKSPACE"'
+                }
+            };
+        }
     }
-  }
 }
